318e15a873b83ec24228cc103c33a25ce7b75620
Robert Snyder <r.snyder49@gmail.com>
2019-01-10 23:59:04 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -19,7 +19,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"value={this.state.value} onChange={this.handlesTwitterMessage} />
       </div>
     );
   }
